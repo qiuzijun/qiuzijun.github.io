4526cdb39a6f391a473795f2708797932a5b720c
qiuzijun <3118897593@qq.com>
2023-11-09 10:49:24 +0800

    fix:修复打包空白bug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import legacy from "@vitejs/plugin-legacy";
+import path from "path";
+import { resolve } from "path";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname);
+  return {
+    plugins: [
+      legacy({
+        targets: ["defaults", "not IE 11"],
+      }),
+      vue(),
+    ],
+    base: "./",
+    resolve: {
+      alias: {
+        //resolve.alias设置别称 解决@绝对路径引入问题
+        "@": path.resolve(__dirname, "src"),
+        "@assets": path.resolve(__dirname, "src/assets"),
+        "@components": path.resolve(__dirname, "src/components"),
+        "@images": path.resolve(__dirname, "src/assets/images"),
+        "@views": path.resolve(__dirname, "src/views"),
+        "@store": path.resolve(__dirname, "src/store"),
+      },
+    },
+  };
 });
